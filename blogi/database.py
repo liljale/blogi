2076826f5d0c0b4638aac183d27091a6f36f4f69
liljale <lilja.l@mailbox.org>
2026-01-24 19:15:36 +0200

    add database wrapper module

---
+++ blogi/database.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+DATABASE = '../database.db'
+
+def get_connection():
+    con = sqlite3.connect(DATABASE)
+    con.row_factory = sqlite3.Row
+    return con
+
+def execute(query, args=(),one=False):
+    con = get_connection()
+    con.execute(query, args)
+    con.commit()
+    con.close()
+
+def query(query, args=(), one=False):
+    con = get_connection()
+    result = con.execute(query, args).fetchall()
+    con.close()
+    return result
