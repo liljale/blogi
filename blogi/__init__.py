f1c7334ffab3ca447505d87a94637d9e6f05fe86
liljale <lilja.l@mailbox.org>
2026-01-25 15:52:58 +0200

    implement login and logout

--- blogi/__init__.py
+++ blogi/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
-from flask import render_template, request, redirect
-from werkzeug.security import generate_password_hash
+from flask import render_template, request, redirect, session
+from werkzeug.security import generate_password_hash, check_password_hash
 
 import sqlite3
 
 from . import db
+from . import config
 
 app = Flask(__name__)
+app.secret_key = config.secret_key
 
 @app.route("/")
 def index():
@@ -23,6 +25,25 @@ def tags():
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def login_post():
+    username = request.form["username"]
+    password = request.form["password"]
+
+    query = "SELECT password_hash FROM users WHERE username = ?"
+    password_hash = db.query(query, [username])[0][0]
+
+    if check_password_hash(password_hash, password):
+        session["username"] = username
+        return redirect("/")
+    else:
+        return "invalid username or password"
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
 
 @app.route("/register")
 def register():
